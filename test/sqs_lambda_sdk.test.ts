d9200eeafaea5de6efde24f041e7ab032937e0eb
katsuyukishimbo <katsuyuki0611@gmail.com>
2022-03-21 16:49:13 +0900

    npx cdk init app --language typescript

---
+++ test/sqs_lambda_sdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsLambdaSdk from '../lib/sqs_lambda_sdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs_lambda_sdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsLambdaSdk.SqsLambdaSdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
